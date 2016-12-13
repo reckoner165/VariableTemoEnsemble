793b0aa797ecca21f98a9e70a6f998b7a172f32d
Qingyang Xi <Qx244@nyu.edu>
2016-12-13 12:44:05 -0500

    Fixed issues with alpha: now alpha would not permit negative tempos. Need better way in the future to pic alpha to ensure the resulting vec stays in the desired range of 30 - 600 bpm. Also fixed crucial bug on best_kernel_mat. Now the images function is correct, and the best_kernel_mat is a lot prettier and meaningful now.

--- mazurka_tempo/beta.m
+++ mazurka_tempo/beta.m
@@ -22,14 +22,14 @@ n_win_size = round(win_dur * fs_sf);
 n_hop_size = round(n_win_size / 2);
 [windowed_n_mat, t_mat, f_rate]  = frame(n_t_sf, t_sf ,fs_sf, n_win_size, n_hop_size);
 
-for col = 1:size(t_mat,2)-60
-    alpha_vec = -0.99:0.1:3;
+for col = 1:size(t_mat,2)-1
+    alpha_vec = -0.9:0.1:3;
     tempo_plane = zeros(length(alpha_vec), size(t_mat, 1));
     best_kernel_mat = tempo_plane;
     for i = 1:length(alpha_vec)
-        alpha = alpha_vec(i);
+        alpha = alpha_vec(i) / win_dur; % make sure negative tempo dont exist
         t_sec = t_mat(:,col);
-        t_warped = warp(t_sec, fs_sf, alpha*f_rate);
+        t_warped = warp(t_sec, fs_sf, alpha);
         if plt == 1 % debugging option
             figure(1)
             plot(t_warped);
@@ -45,13 +45,13 @@ for col = 1:size(t_mat,2)-60
         best_kernel_recovered = interp1(t_warped_0offset, best_kernel_warped, t_sec_0offset, 'spline');
         best_kernel_mat(i, :) = best_kernel_recovered;
     end
-    %figure(1)
-    %imagesc(tempo_plane);
+    figure(1)
+    imagesc(tempo_plane);
     figure(2)
     imagesc(best_kernel_mat);
     disp(col);
     %% get the index of the max element in tempo_plane, in terms of alpha and f.
-    % pause();
+    pause(0.05);
 end
 
 % %     imagesc(tempo_plane);
