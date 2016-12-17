af9c83998103acca25efb70c4e52798032e54152
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2016-12-17 16:00:15 -0500

    All viterbi parameters

--- mazurka_tempo/beta_sumanth.m
+++ mazurka_tempo/beta_sumanth.m
@@ -77,8 +77,11 @@ for col = 1:size(t_mat,2)-1
     [Y(col,:), ~] = plp(n_sf_frame, fs_sf, f_basis);
     all_best_alpha(col) = best_alpha;
 
-%     Building Transition Matrix for each frame
-
+% Building Viterbi Parameters
+% ---------------------------
+    
+% Building Transition Matrix for each frame
+% (Will replace this block by plugging function)
     for w = 1:size(tempo_plane,2)
         wi_prob = tempo_plane(:,w);
         k = 1.5;
@@ -91,6 +94,25 @@ for col = 1:size(t_mat,2)-1
     end
     imagesc(transition);
     drawnow;
+% P[y|S] is the plp for 0 alpha
+    [emission_prob, ~] = plp(n_sf_frame, fs_sf, f_basis);
+    
+% Observation for each frame is the tempo plane for that frame
+    observation = tempo_plane;
+
+% Initial Probability is a uniform distribution
+    pi_w = ones(length(f_basis),1)/length(f_basis);
+    
+% VITERBI
+% -------
+
+if col == 1
+%    Initial step 
+
+else
+%     Get path and best i
+end
+    
 end
 
 % unframe:
@@ -101,19 +123,7 @@ gamma_t = unframe(gamma_mat, n_hop_size);
 
 %%
 
-% viterbi
-% % % 
-% subplot(2,1,1), imagesc(log(abs(Y)')), colorbar; title('Y matrix')
-% hold on;
-% plot(all_best_alpha,'.r');
-% subplot(2,1,2), imagesc(log(S')),colorbar; title('S matrix')
 
-% Initial pi(alpha) is a uniform/random distribution to start with
-PI_alpha = ones(length(alpha_vec),1);
-% Uniform transition matrix ***FOR NOW***
-trans = ones(length(alpha_vec)); 
-
-% Need to compute all state probabilities
 % 
 % figure(1)
 % plot(gamma_t);
